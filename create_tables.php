f91d0700df6d6041d1368fbc919c5002b08b986d
joel gustavo suarez rodriguez <joek_tavo@hotmail.com>
2018-11-27 14:26:41 -0500

    Deleting and updating in postgreSQL

--- create_tables.php
+++ create_tables.php
@@ -17,18 +17,34 @@
     {
         echo "Ya hizo una insercion";
         // exit('\nInsert already done');
+    }else 
+    {
+        $sql = "INSERT INTO books (author, title, year) VALUES ('Al Johnson', 'Mark The Messenger', 1983);
+        INSERT INTO books (author, title, year) VALUES ('George Hamilton', 'Rig The Crime', 1985);
+        INSERT INTO books (author, title, year) VALUES ('Rod Peterson', 'Innocent Child', 1975);
+        INSERT INTO books (author, title, year) VALUES ('June Landers', 'Better Way', 1977);
+        INSERT INTO books (author, title, year) VALUES ('Burt Fender', 'On The Job', 1988)";
+
+        $pg->insert($sql);
+        echo 'Insert Done';
     }
-    $sql = "INSERT INTO books (author, title, year) VALUES ('Al Johnson', 'Mark The Messenger', 1983);
-            INSERT INTO books (author, title, year) VALUES ('George Hamilton', 'Rig The Crime', 1985);
-            INSERT INTO books (author, title, year) VALUES ('Rod Peterson', 'Innocent Child', 1975);
-            INSERT INTO books (author, title, year) VALUES ('June Landers', 'Better Way', 1977);
-            INSERT INTO books (author, title, year) VALUES ('Burt Fender', 'On The Job', 1988)";
-    $pg->insert($sql);
-    echo 'Insert Done';
     
-    $sql = "SELECT year FROM books WHERE title='On The Job'";
+    $sql = "SELECT * FROM books WHERE title='Better Way'";
     $author = $pg->getCol($sql);
+
+    $varToFind = 'Better Way';
+    $sql = "SELECT * FROM books WHERE title='$varToFind'";
+    $authorB = $pg->getRow($sql);
     echo '\n\nprocess succesfull';
+
+    //deleting all books
+    // $sql = "DELETE FROM books RETURNING *";
+    // $pg->exec($sql);
+
+    //updating rows in book.
+    $sql = "UPDATE books SET title = 'Better Way that' WHERE title = 'Better Way'";
+    $pg->exec($sql);
+    echo "Update data table";
 
     
 
