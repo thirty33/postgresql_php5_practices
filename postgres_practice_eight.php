1025c9908638103d65364737d084d110aa662373
joel gustavo suarez rodriguez <joek_tavo@hotmail.com>
2018-12-06 17:58:11 -0500

    utlizando orderBy para hacer consultas

--- postgres_practice_eight.php
+++ postgres_practice_eight.php
@@ -15,6 +15,27 @@
     
     $sql = "INSERT INTO persona (nombre) values ('joel')";
     $pg->exec($sql);
+    $sql = "INSERT INTO persona (nombre, serial_motor) values ('joel', 15487)";
+    $pg->exec($sql);
+    $sql = "INSERT INTO persona (nombre, serial_motor) values ('esteban', 15487)";
+    $pg->exec($sql);
+    $sql = "INSERT INTO persona (nombre, serial_motor) values ('danilo', 15487)";
+    $pg->exec($sql);
+
+    $str_array = array('joel', 'steban', 'danilo');
+    foreach ($str_array as $name) {
+        echo "this is the name $name";
+    }
+
+    // asc is default in order by sentence
+    // $sql = "SELECT * FROM persona order by nombre, serial_motor asc";
+    // $sql = "SELECT * FROM persona order by nombre desc";
+    // $vehi = $pg->getRow($sql);
+    // echo $vehi;
+    
+    $sql = "SELECT * FROM persona where nombre like '%teb%'";
+    $vehi = $pg->getRows($sql);
+    echo $vehi;
 
 
 ?>
